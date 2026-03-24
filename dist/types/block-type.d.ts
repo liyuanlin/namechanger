15540644a0fdb39b426dbff7c960a8ef99b653ad
Larry Yuanlin Li <liyuanlin@qq.com>
2026-03-24 09:49:47 +0800

    remove dist

--- dist/types/block-type.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum BlockType {
-    Unknown = 0,
-    Comments = 1,
-    Value = 2,
-    Slice = 3
-}
-//# sourceMappingURL=block-type.d.ts.map
--- dist/types/value-types.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum ValueType {
-    Unknown = 0,
-    InnerTextValue = 1,
-    AttributeValue = 2,
-    FormatValue = 3
-}
-//# sourceMappingURL=value-types.d.ts.map
